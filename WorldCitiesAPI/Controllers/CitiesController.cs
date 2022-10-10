ce066cfc2e262c2f0a2d6dfa69d17abc86f27109
Auguste Dubuisson <guy32807@comcast.net>
2022-10-10 18:46:58 -0400

    New functionalities

--- WorldCitiesAPI/Controllers/CitiesController.cs
+++ WorldCitiesAPI/Controllers/CitiesController.cs
@@ -19,10 +19,17 @@ namespace WorldCitiesAPI.Controllers
 
         // GET: api/Cities
         [HttpGet]
-        public async Task<ActionResult<ApiResult<City>>> GetCities(int pageIndex = 0, int pageSize = 10, string? sortColumn=null, string? sortOrder = null, string? filterColumn = null, string? filterQuery = null)
+        public async Task<ActionResult<ApiResult<CityDTO>>> GetCities(int pageIndex = 0, int pageSize = 10, string? sortColumn=null, string? sortOrder = null, string? filterColumn = null, string? filterQuery = null)
         {
-            return await ApiResult<City>.CreateAsync(
-            _context.Cities.AsNoTracking(),
+            return await ApiResult<CityDTO>.CreateAsync(
+            _context.Cities.AsNoTracking().Select(c => new CityDTO() {
+                Id = c.Id,
+                Name = c.Name,
+                Lat = c.Lat,
+                Lon = c.Lon,
+                CountryId = c.Country!.Id,
+                CountryName = c.Country!.Name
+            }),
             pageIndex,
             pageSize,
             sortColumn,
--- WorldCitiesAPI/Controllers/CountriesController.cs
+++ WorldCitiesAPI/Controllers/CountriesController.cs
@@ -24,9 +24,17 @@ namespace WorldCitiesAPI.Controllers
 
         // GET: api/Countries
         [HttpGet]
-        public async Task<ActionResult<ApiResult<Country>>> GetCountries(int pageIndex = 0, int pageSize = 10, string? sortColumn = null, string? sortOrder = null, string? filterColumn = null, string? filterQuery = null)
+        public async Task<ActionResult<ApiResult<CountryDTO>>> GetCountries(int pageIndex = 0, int pageSize = 10, string? sortColumn = null, string? sortOrder = null, string? filterColumn = null, string? filterQuery = null)
         {
-            return await ApiResult<Country>.CreateAsync(_context.Countries.AsNoTracking(), pageIndex, pageSize, sortColumn, sortOrder, filterColumn, filterQuery);
+            return await ApiResult<CountryDTO>.CreateAsync(_context.Countries.AsNoTracking()
+                .Select(c => new CountryDTO()
+                {
+                    Id = c.Id,
+                    Name = c.Name,
+                    ISO2 = c.ISO2,
+                    ISO3 = c.ISO3,
+                    TotCities = c.Cities!.Count
+                }), pageIndex, pageSize, sortColumn, sortOrder, filterColumn, filterQuery);
         }
 
         // GET: api/Countries/5
